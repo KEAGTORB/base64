954e2540a6fd4814ab0d4472bffefe117ce6f785
Minhaj <92469140+KEAGTORB@users.noreply.github.com>
2022-01-08 11:45:35 +0530

    Add files via upload

---
+++ install.py
@@ -0,0 +1,30 @@
+#bin/python
+#Author: KEAGTORB
+import os
+import time
+import sys
+import colorama
+
+CY= '\033[1;34m'
+B= '\033[0;90m'
+S= '\033[0;91m'
+C= '\033[1;37m'
+ki= '\033[0;60m'
+about= "\033[0;89mThis tool will help you in installing required packages for my tool"
+#Start now
+os.system("cls || clear") 
+print(CY+ "This script will help you in installing required packages for base64 bash script") 
+time.sleep(3)
+os.system("pkg install git") 
+os.system("pip install lolcat") 
+os.system("pip install figlet") 
+os.system("pkg install wget && pkg install curl && pkg install clang && pkg install unzip") 
+os.system("cd $HOME") 
+os.system("cd base64") 
+print(S+ "All packages have been successfully installed ") 
+print("") 
+print(C+ "Thanks for using this tool made by KEAGTORB")
+print(B+ "Now wait for 3 secs while the tool starts....")
+time.sleep(3)
+os.system("bash encode.sh") 
+#IAM KEAGTORB
